a6c8efcd117eac970420c8c64ea9a3e3526704b6
swapnil <swapnil.suhane@gmail.com>
2023-06-30 08:42:11 +0800

    PrintBit

---
+++ codeme/src/main/java/com/just/code/code/dsalgo/bitops/PrintBit.java
@@ -0,0 +1,16 @@
+package com.just.code.code.dsalgo.bitops;
+
+public class PrintBit {
+
+    public static void main(String[] args) {
+        printTwoPowerBits();
+    }
+
+    private static void printTwoPowerBits() {
+        System.out.println("print all binary for power of two");
+        for(int i=0; i<=62; i++) {
+            long k = 1L <<i;
+            System.out.println("No: " + i + " power: " + k + " binary: "+ Long.toBinaryString(k) + " bitCount: " + Long.bitCount(k));
+        }
+    }
+}
