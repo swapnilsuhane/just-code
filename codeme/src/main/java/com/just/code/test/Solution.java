0eb05503add1143260ec1ed9af7d79f7bd869080
swapnilsuhane <swapnil.suhane@fairpricegroup.sg>
2024-10-26 13:03:40 +0800

    added Solution

---
+++ codeme/src/main/java/com/just/code/test/Solution.java
@@ -0,0 +1,12 @@
+package com.just.code.test;
+
+class Solution {
+    public static void print() {
+        System.out.println("Hello Again!");
+    }
+
+    public static void main(String[] str) {
+        print();
+        System.out.println("Restart!!");
+    }
+}
